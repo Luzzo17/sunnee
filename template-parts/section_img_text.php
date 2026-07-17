863dc51c5168cb7830450d0d67f7f575ba9f7f03
Luzzo17 <ferrucciangelo03@gmail.com>
2026-07-17 12:35:05 +0200

    Mobile responsive

--- template-parts/section_img_text.php
+++ template-parts/section_img_text.php
@@ -1,9 +1,9 @@
 <div class="section">
     <h2><?php echo esc_html(get_theme_mod('story_title', 'STORIA')); ?></h2>
-    <div class="section_img">
+    <div class="section_img" id="imgtxt">
         <div class="side_img">
     <img src="<?php echo esc_url(get_theme_mod('story_image', get_theme_file_uri('assets/images/story.jpg'))); ?>" alt="">
 </div>
-<p style="text-align: right"><?php echo esc_html(get_theme_mod('story_text', 'Nel 2018 la fondatrice ha lanciato Sunnee con l obiettivo di creare un brand che riflettesse i suoi valori di sportiva e amante del mare. Oggi Sunnee e un team strutturato nel settore del beachwear sostenibile.')); ?></p>
+<p><?php echo esc_html(get_theme_mod('story_text', 'Nel 2018 la fondatrice ha lanciato Sunnee con l obiettivo di creare un brand che riflettesse i suoi valori di sportiva e amante del mare. Oggi Sunnee e un team strutturato nel settore del beachwear sostenibile.')); ?></p>
     </div>
 </div>
--- template-parts/section_text_img.php
+++ template-parts/section_text_img.php
@@ -1,7 +1,7 @@
 <div class="section">
     <h2><?php echo esc_html(get_theme_mod('vision_title', 'VISION')); ?></h2>
-    <div class="section_img">
-<p style="text-align: left"><?php echo esc_html(get_theme_mod('vision_text', 'Innovare il settore del beachwear sostenibile utilizzando esclusivamente filati ricavati da plastica riciclata.')); ?></p>
+    <div class="section_img" id="txtimg">
+<p><?php echo esc_html(get_theme_mod('vision_text', 'Innovare il settore del beachwear sostenibile utilizzando esclusivamente filati ricavati da plastica riciclata.')); ?></p>
     <div class="side_img">
     <img src="<?php echo esc_url(get_theme_mod('vision_image', get_theme_file_uri('assets/images/vision.jpg'))); ?>" alt="">
 </div>
